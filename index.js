f75d96b93f28251b53a7492d62a7f6c4118d8611
Extiint <115897446+Extiint@users.noreply.github.com>
2023-07-21 11:50:56 +0000

    comm

---
+++ index.js
@@ -0,0 +1,124 @@
+//const { contractAbi, contractAddress } = require('./web3/Abitest');
+const { contractAbi, contractAddress } = require('./web3/Abi');
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 49152
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  
+require('dotenv').config();
+const { ethers } = require('ethers');
+const Big = require('big.js');
+
+const httpProvider = new ethers.JsonRpcProvider(process.env.JSON_RPC);
+
+
+const contract = new ethers.Contract(contractAddress, contractAbi, httpProvider);
+
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: { interval: 10000 } });
+//const targetGroupId = '-100674732483';
+const targetGroupId = '-1001275249191'; 
+
+
+bot.on('polling_error', (err) => {
+  console.error('Polling error:', err);
+});
+
+
+const axios = require('axios');
+
+  const buySignature ='0xdb663865';
+
+  const apiKey = 'Z1B3GRG3NXG82SA5P6GKMHBF1JPRCGFQP8'; // Replace with your BscScan API key
+  let lastTransactionTimestamp = 1685463722;
+  const alreadyNotified = new Set();
+
+
+async function fetchLatestDepositTransaction(contractAddress, depositSignature, bot, targetGroupId) {
+  try {
+    // Fetch the current value of totalDeposits from Firestore:    
+    
+    const response = await axios.get(
+      `https://api.bscscan.com/api?module=account&action=txlist&address=${contractAddress}&startblock=0&endblock=99999999&sort=desc&apikey=${apiKey}`
+    );
+
+    const transactions = response.data.result;
+    const depositTransactions = transactions.filter((tx) =>
+    tx.methodId === buySignature && 
+    parseInt(tx.timeStamp) > lastTransactionTimestamp &&
+  parseInt(tx.timeStamp) > Math.floor(Date.now() / 1000) - 60
+);
+
+    if (depositTransactions.length === 0) return;
+    const sortedTransactions = depositTransactions.sort((a, b) => parseInt(a.timeStamp) - parseInt(b.timeStamp));
+    console.log(sortedTransactions,"hereeee");
+
+    const latestTransaction = sortedTransactions[sortedTransactions.length -1];
+
+    lastTransactionTimestamp = parseInt(latestTransaction.timeStamp);
+
+    console.log('Latest deposit transactio:', latestTransaction);
+    if (alreadyNotified.has(latestTransaction.hash)) return;
+    alreadyNotified.add(latestTransaction.hash);
+
+
+    const abiCoder = new ethers.AbiCoder();
+    const inputData = latestTransaction.input.slice(10);
+    const decodedData = abiCoder.decode(
+        ['address'], // Update this to match the inputs of your deposit function
+        '0x' + inputData
+      );
+      
+
+      const decodedTransaction = {
+        hash: latestTransaction.hash,
+        from: decodedData[0],
+        amount: latestTransaction.value
+      };
+      
+      console.log(decodedTransaction)
+
+
+    const newamount = new Big(Number(decodedTransaction.amount)).div(new Big('1e18')).toFixed(2);
+
+    const totalDeposits = await contract.getBalance();
+
+
+    const bscScanLink = `https://bscscan.com/tx/${latestTransaction.hash}`;
+    console.log(bscScanLink);
+    const totalDeposits2 = new Big(Number(totalDeposits)).div(new Big('1e18')).toFixed(2);
+    let numEmojis;
+
+    // calculate number of emojis to send
+    if (newamount >= 60) {
+      numEmojis = Math.floor(60 * 4);
+    } else {
+      numEmojis = Math.floor(newamount * 100);
+    }
+    let emojis = '';
+    for (let i = 0; i < numEmojis; i++) {
+      emojis += '⭐'; // add rocket emoji
+    }
+    const caption = `<b>Classic Miner new deposit!</b>\n\n${emojis} \n\n<b>TVL:</b> ${totalDeposits2} USDT\n<b>Deposit Amount:</b> ${newamount} USDT\n\n  <a href="https://classicminer.io/"><u>Website</u></a>  |  <a href="https://the-stamp.com/2023/07/classic-miner/"><u>Audit</u></a>  |  <a href=${bscScanLink}><u>Tx</u></a>`;
+    await bot.sendPhoto(targetGroupId, 'https://ipfs.filebase.io/ipfs/QmSc3Xq7TYxuDHArvi9RDLdgrjDmVTGC7ugb9hnyuvPif4', {
+      caption: caption,
+      parse_mode: 'HTML'
+    });
+
+  } catch (error) {
+    console.error('Error fetching BscScan API:', error.message);
+    console.log('Response data:', response.data);
+  }
+}
+
+setInterval(() => {
+  fetchLatestDepositTransaction(contractAddress, buySignature, bot, targetGroupId);
+  }, 1000); // 10 seconds interval
---
+++ web3/Abi.js
@@ -0,0 +1,458 @@
+
+  // Abi.js
+
+  const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_rec2",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_mkt",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_mkt2",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "JACKPOT",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "LIMIT_ROI",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "MIN_DEPOSIT_TO_WIN",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "RENOUNCE_FEE_DATE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "TIME_JACK",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "WINNER",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "adr",
+				"type": "address"
+			}
+		],
+		"name": "beanRewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "ref",
+				"type": "address"
+			}
+		],
+		"name": "buyEggs",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "eth",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "contractBalance",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateEggBuy",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "eth",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateEggBuySimple",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "eggs",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateEggSell",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "eggs",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateoldEggSell",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "dumpedEggs",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "adr",
+				"type": "address"
+			}
+		],
+		"name": "getEggsSinceLastHatch",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "adr",
+				"type": "address"
+			}
+		],
+		"name": "getMyEggs",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "adr",
+				"type": "address"
+			}
+		],
+		"name": "getMyMiners",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getWinnerInfo",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "hatchEggs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "isContract",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isLaunched",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "marketEggs",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "returnLas2t",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "returnLast",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "returnROI",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "sellEggs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "adr",
+				"type": "address"
+			}
+		],
+		"name": "tiredMiners",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+        
+        const contractAddress = '0x6f7D4914d73993955d3F95E396b7bb7f964b2D2c';
+  
+        module.exports = { contractAbi, contractAddress };
+        
